8191c91f2deda4c90630c629a65b91daf3a6f9df
vibudesh07 <vibudeshrb.22cse@kongu.edu>
2025-04-26 12:47:19 +0530

    master

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -12,7 +12,7 @@ class ChatbotController {
             replyMessage = 'I am not sure how to respond to that.';
         }
 
-        res.json({ reply: replyMessage });
+        res.send(replyMessage );
     }
 }
 
