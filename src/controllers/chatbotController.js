652f55c67be8de27c655d871f5d9eb92e8d2783a
vibudesh07 <vibudeshrb.22cse@kongu.edu>
2025-04-26 13:09:48 +0530

    master

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -1,20 +1,26 @@
 class ChatbotController {
     handleIncomingMessage(req, res) {
-        const userMessage = req.body.message;
+       
+        const userMessage = req.body.Body; 
+   
+
         let replyMessage;
 
-     
-        if (userMessage.includes('hello')) {
+        if (userMessage.toLowerCase().includes('hello')) {
             replyMessage = 'Hello! How can I assist you today?';
-        } else if (userMessage.includes('help')) {
+        } else if (userMessage.toLowerCase().includes('help')) {
             replyMessage = 'Sure! What do you need help with?';
         } else {
             replyMessage = 'I am not sure how to respond to that.';
         }
 
-        res.send(replyMessage );
+        res.set('Content-Type', 'text/xml'); 
+        res.send(`
+            <Response>
+                <Message>${replyMessage}</Message>
+            </Response>
+        `);
     }
 }
 
 module.exports = ChatbotController;
-
--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ const chatbotRoutes = require('./routes/chatbotRoutes');
 const app = express();
 const port = 8000;
 
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api', chatbotRoutes);
 
 app.listen(port, () => {
